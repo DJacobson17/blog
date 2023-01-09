14b681f7e009148129f398421d253313ba74bbb0
DJacobson17 <davidbjacobson@gmail.com>
2023-01-09 14:06:09 -0600

    switched db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_08_232800) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_08_232800) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
+    t.text "commenter"
     t.text "body"
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
