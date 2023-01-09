652b61f6a5ce996fea213069dc8dd5651ec53a56
DJacobson17 <davidbjacobson@gmail.com>
2023-01-08 19:28:05 -0600

    added comments section

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'articles#index'
 
-  resources :articles
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles do
+    resources :comments
+  end
 end
